fb3ff204b25d4cf85aa8b95b80055b87c91deb6c
DigitalCrafting <k.kasprzyk2@gmail.com>
2026-02-26 21:57:43 +0100

    [@zoria-ui] Added more icons and Grid

--- packages/zoria/react/src/components/icons/Icons.tsx
+++ packages/zoria/react/src/components/icons/Icons.tsx
@@ -5,7 +5,7 @@ interface SvgIconBaseProps extends React.SVGProps<SVGSVGElement> {
     size?: UiSize
 }
 
-export function SvgIconBase({children, ...props}: SvgIconBaseProps) {
+export const SvgIconBase = ({children, ...props}: SvgIconBaseProps) => {
     return (<svg
         viewBox="0 0 24 24"
         fill="none"
@@ -21,38 +21,38 @@ export function SvgIconBase({children, ...props}: SvgIconBaseProps) {
     </svg>);
 }
 
-export function XIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const XIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="M6 18L18 6"/>
         <path d="M6 6l12 12"/>
     </SvgIconBase>
 }
 
-export function ChevronDownIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const ChevronDownIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="m6 9 6 6 6-6"/>
     </SvgIconBase>
 }
 
-export function ChevronLeftIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const ChevronLeftIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="m15 18-6-6 6-6"/>
     </SvgIconBase>
 }
 
-export function ChevronRightIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const ChevronRightIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="m9 18 6-6-6-6"/>
     </SvgIconBase>
 }
 
-export function ChevronUpIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const ChevronUpIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="m18 15-6-6-6 6"/>
     </SvgIconBase>
 }
 
-export function CalendarIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const CalendarIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="M8 2v4"/>
         <path d="M16 2v4"/>
@@ -61,7 +61,7 @@ export function CalendarIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
     </SvgIconBase>
 }
 
-export function CalendarCheckIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const CalendarCheckIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="M8 2v4"/>
         <path d="M16 2v4"/>
@@ -71,14 +71,14 @@ export function CalendarCheckIcon({size = UiSize.MD, ...props}: SvgIconBaseProps
     </SvgIconBase>
 }
 
-export function ClockIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const ClockIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="M12 6v6l4 2"/>
         <circle cx="12" cy="12" r="10"/>
     </SvgIconBase>
 }
 
-export function ClockCheckIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const ClockCheckIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="M12 6v6l4 2"/>
         <path d="M22 12a10 10 0 1 0-11 9.95"/>
@@ -86,20 +86,20 @@ export function ClockCheckIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
     </SvgIconBase>
 }
 
-export function CheckIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const CheckIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="M20 6 9 17l-5-5"/>
     </SvgIconBase>
 }
 
-export function CopyIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const CopyIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <rect width="14" height="14" x="8" y="8" rx="2" ry="2"/>
         <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/>
     </SvgIconBase>
 }
 
-export function CopyCheckIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const CopyCheckIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="m12 15 2 2 4-4"/>
         <rect width="14" height="14" x="8" y="8" rx="2" ry="2"/>
@@ -107,7 +107,7 @@ export function CopyCheckIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
     </SvgIconBase>
 }
 
-export function MenuIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const MenuIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <path d="M4 5h16"/>
         <path d="M4 12h16"/>
@@ -115,7 +115,7 @@ export function MenuIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
     </SvgIconBase>
 }
 
-export function EllipsisMenuIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const EllipsisMenuIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <circle cx="12" cy="12" r="1"/>
         <circle cx="12" cy="5" r="1"/>
@@ -123,11 +123,223 @@ export function EllipsisMenuIcon({size = UiSize.MD, ...props}: SvgIconBaseProps)
     </SvgIconBase>
 }
 
-export function EllipsisIcon({size = UiSize.MD, ...props}: SvgIconBaseProps) {
+export const EllipsisIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
     return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
         <circle cx="12" cy="12" r="1"/>
         <circle cx="19" cy="12" r="1"/>
         <circle cx="5" cy="12" r="1"/>
     </SvgIconBase>
 }
+
+
+export const DatabaseIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <ellipse cx="12" cy="5" rx="9" ry="3"/>
+        <path d="M3 5V19A9 3 0 0 0 21 19V5"/>
+        <path d="M3 12A9 3 0 0 0 21 12"/>
+    </SvgIconBase>
+}
+
+export const DatabaseBackupIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <ellipse cx="12" cy="5" rx="9" ry="3"/>
+        <path d="M3 12a9 3 0 0 0 5 2.69"/>
+        <path d="M21 9.3V5"/>
+        <path d="M3 5v14a9 3 0 0 0 6.47 2.88"/>
+        <path d="M12 12v4h4"/>
+        <path d="M13 20a5 5 0 0 0 9-3 4.5 4.5 0 0 0-4.5-4.5c-1.33 0-2.54.54-3.41 1.41L12 16"/>
+    </SvgIconBase>
+}
+
+export const DatabaseSearchIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path d="M21 11.693V5"/>
+        <path d="m22 22-1.875-1.875"/>
+        <path d="M3 12a9 3 0 0 0 8.697 2.998"/>
+        <path d="M3 5v14a9 3 0 0 0 9.28 2.999"/>
+        <circle cx="18" cy="18" r="3"/>
+        <ellipse cx="12" cy="5" rx="9" ry="3"/>
+    </SvgIconBase>
+}
+
+export const ServerIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <rect width="20" height="8" x="2" y="2" rx="2" ry="2"/>
+        <rect width="20" height="8" x="2" y="14" rx="2" ry="2"/>
+        <line x1="6" x2="6.01" y1="6" y2="6"/>
+        <line x1="6" x2="6.01" y1="18" y2="18"/>
+    </SvgIconBase>
+}
+
+export const ServerCogIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path d="m10.852 14.772-.383.923"/>
+        <path d="M13.148 14.772a3 3 0 1 0-2.296-5.544l-.383-.923"/>
+        <path d="m13.148 9.228.383-.923"/>
+        <path d="m13.53 15.696-.382-.924a3 3 0 1 1-2.296-5.544"/>
+        <path d="m14.772 10.852.923-.383"/>
+        <path d="m14.772 13.148.923.383"/>
+        <path d="M4.5 10H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-.5"/>
+        <path d="M4.5 14H4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-.5"/>
+        <path d="M6 18h.01"/>
+        <path d="M6 6h.01"/>
+        <path d="m9.228 10.852-.923-.383"/>
+        <path d="m9.228 13.148-.923.383"/>
+    </SvgIconBase>
+}
+
+export const ServerCrashIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path d="M6 10H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-2"/>
+        <path d="M6 14H4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2"/>
+        <path d="M6 6h.01"/>
+        <path d="M6 18h.01"/>
+        <path d="m13 6-4 6h6l-4 6"/>
+    </SvgIconBase>
+}
+
+export const ServerOffIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path d="M7 2h13a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-5"/>
+        <path d="M10 10 2.5 2.5C2 2 2 2.5 2 5v3a2 2 0 0 0 2 2h6z"/>
+        <path d="M22 17v-1a2 2 0 0 0-2-2h-1"/>
+        <path d="M4 14a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16.5l1-.5.5.5-8-8H4z"/>
+        <path d="M6 18h.01"/>
+        <path d="m2 2 20 20"/>
+    </SvgIconBase>
+}
+
+export const HardDriveIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path d="M10 16h.01"/>
+        <path
+            d="M2.212 11.577a2 2 0 0 0-.212.896V18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-5.527a2 2 0 0 0-.212-.896L18.55 5.11A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"/>
+        <path d="M21.946 12.013H2.054"/>
+        <path d="M6 16h.01"/>
+    </SvgIconBase>
+}
+
+export const HardDriveDownloadIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path d="M12 2v8"/>
+        <path d="m16 6-4 4-4-4"/>
+        <rect width="20" height="8" x="2" y="14" rx="2"/>
+        <path d="M6 18h.01"/>
+        <path d="M10 18h.01"/>
+    </SvgIconBase>
+}
+
+export const HardDriveUploadIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path d="m16 6-4-4-4 4"/>
+        <path d="M12 2v8"/>
+        <rect width="20" height="8" x="2" y="14" rx="2"/>
+        <path d="M6 18h.01"/>
+        <path d="M10 18h.01"/>
+    </SvgIconBase>
+}
+
+
+export const MonitorIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <rect width="20" height="14" x="2" y="3" rx="2"/>
+        <line x1="8" x2="16" y1="21" y2="21"/>
+        <line x1="12" x2="12" y1="17" y2="21"/>
+    </SvgIconBase>
+}
+
+export const LaptopIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path
+            d="M18 5a2 2 0 0 1 2 2v8.526a2 2 0 0 0 .212.897l1.068 2.127a1 1 0 0 1-.9 1.45H3.62a1 1 0 0 1-.9-1.45l1.068-2.127A2 2 0 0 0 4 15.526V7a2 2 0 0 1 2-2z"/>
+        <path d="M20.054 15.987H3.946"/>
+    </SvgIconBase>
+}
+
+export const SmartphoneIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <rect width="14" height="20" x="5" y="2" rx="2" ry="2"/>
+        <path d="M12 18h.01"/>
+    </SvgIconBase>
+}
+
+export const WifiIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path d="M12 20h.01"/>
+        <path d="M2 8.82a15 15 0 0 1 20 0"/>
+        <path d="M5 12.859a10 10 0 0 1 14 0"/>
+        <path d="M8.5 16.429a5 5 0 0 1 7 0"/>
+    </SvgIconBase>
+}
+
+export const RouterIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <rect width="20" height="8" x="2" y="14" rx="2"/>
+        <path d="M6.01 18H6"/>
+        <path d="M10.01 18H10"/>
+        <path d="M15 10v4"/>
+        <path d="M17.84 7.17a4 4 0 0 0-5.66 0"/>
+        <path d="M20.66 4.34a8 8 0 0 0-11.31 0"/>
+    </SvgIconBase>
+}
+
+export const SatelliteDishIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path d="M4 10a7.31 7.31 0 0 0 10 10Z"/>
+        <path d="m9 15 3-3"/>
+        <path d="M17 13a6 6 0 0 0-6-6"/>
+        <path d="M21 13A10 10 0 0 0 11 3"/>
+    </SvgIconBase>
+}
+
+export const NetworkIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <rect x="16" y="16" width="6" height="6" rx="1"/>
+        <rect x="2" y="16" width="6" height="6" rx="1"/>
+        <rect x="9" y="2" width="6" height="6" rx="1"/>
+        <path d="M5 16v-3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3"/>
+        <path d="M12 12V8"/>
+    </SvgIconBase>
+}
+
+export const TriangleAlertIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3"/>
+        <path d="M12 9v4"/>
+        <path d="M12 17h.01"/>
+    </SvgIconBase>
+}
+
+export const CircleAlertIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <circle cx="12" cy="12" r="10"/>
+        <line x1="12" x2="12" y1="8" y2="12"/>
+        <line x1="12" x2="12.01" y1="16" y2="16"/>
+    </SvgIconBase>
+}
+
+export const CircleCheckIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <circle cx="12" cy="12" r="10"/>
+        <path d="m9 12 2 2 4-4"/>
+    </SvgIconBase>
+}
+
+export const CircleQuestionIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <circle cx="12" cy="12" r="10"/>
+        <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/>
+        <path d="M12 17h.01"/>
+    </SvgIconBase>
+}
+
+export const CircleInfoIcon = ({size = UiSize.MD, ...props}: SvgIconBaseProps) => {
+    return <SvgIconBase className={`z-icon z-icon-${size}`} {...props}>
+        <circle cx="12" cy="12" r="10"/>
+        <path d="M12 16v-4"/>
+        <path d="M12 8h.01"/>
+    </SvgIconBase>
+}
+
+
 
--- packages/zoria/react/src/components/layout/Layout.tsx
+++ packages/zoria/react/src/components/layout/Layout.tsx
@@ -5,20 +5,51 @@ interface RowProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
     gap?: UiSize
 }
 
-export function Row({children, gap = UiSize.MD, className: externalClassName = '', ...rest}: React.PropsWithChildren<RowProps>) {
+export function Row({
+    children,
+    gap = UiSize.MD,
+    className: externalClassName = '',
+    ...rest
+}: React.PropsWithChildren<RowProps>) {
     return <div className={`z-row z-row-${gap} ${externalClassName}`} {...rest}>{children}</div>
 }
 
-interface BstColProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
+interface ColProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
     gap?: UiSize;
     span?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 }
 
-export function Col({children, span = undefined, gap = UiSize.MD, className: externalClassName = '', ...rest}: React.PropsWithChildren<BstColProps>) {
+export function Col({
+    children,
+    span = undefined,
+    gap = UiSize.MD,
+    className: externalClassName = '',
+    ...rest
+}: React.PropsWithChildren<ColProps>) {
     let colClassName = `z-col z-col-${gap} `;
     if (span) {
         colClassName += 'z-col-' + span;
     }
 
     return <div className={`${colClassName} ${externalClassName}`} {...rest}>{children}</div>
+}
+
+interface GridProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
+    gap?: UiSize;
+    cols?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+}
+
+export function Grid({
+    children,
+    cols = 1,
+    gap = UiSize.MD,
+    className: externalClassName = '',
+    ...rest
+}: GridProps) {
+    let colClassName = `z-grid z-grid-${gap} `;
+    if (cols) {
+        colClassName += 'z-grid-' + cols;
+    }
+
+    return <div className={`${colClassName} ${externalClassName}`} {...rest}>{children}</div>;
 }
