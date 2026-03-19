d5a131b7f4dff4041afa5acb0df8724d28560733
DigitalCrafting <k.kasprzyk2@gmail.com>
2026-03-19 21:09:35 +0100

    [@zoria-ui] Proper general tokens

--- apps/digital-crafting/src/components/DCHeader.tsx
+++ apps/digital-crafting/src/components/DCHeader.tsx
@@ -6,7 +6,7 @@ export const DCHeader = () => {
         <TopBar>
             <Container>
                 <Row>
-                    <Link className='nav-link' to="/"><Text>Main</Text></Link>
+                    <Link className='nav-link' to="/"><Text>Home</Text></Link>
                     <Link className='nav-link' to="/zoria"><Text>Zoria UI</Text></Link>
                 </Row>
             </Container>
