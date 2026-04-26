836c6e2955638471b1167d4dd0d8b5ad1e94500d
DigitalCrafting <k.kasprzyk2@gmail.com>
2026-04-26 11:39:00 +0200

    [digital-crafting] Hide theme toggle on landing page

--- apps/digital-crafting/src/components/DCHeader.tsx
+++ apps/digital-crafting/src/components/DCHeader.tsx
@@ -1,18 +1,19 @@
 import {Col, Container, Layout, Row, Text, Toggle, TopBar} from "@zoria-ui/react";
-import {Link, useLocation} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {useIsRootRoute} from "../routes/useIsRootRoute.tsx";
 
 export const DCHeader = () => {
     const [darkThemeEnabled, setDarkThemeEnabled] = useState(false);
-    const location = useLocation();
+    const isRoot = useIsRootRoute();
 
     useEffect(() => {
-        if (darkThemeEnabled && location.pathname !== '/') {
+        if (darkThemeEnabled && !isRoot) {
             document.documentElement.setAttribute('data-theme', 'dark');
         } else {
             document.documentElement.removeAttribute('data-theme');
         }
-    }, [darkThemeEnabled, location]);
+    }, [darkThemeEnabled, isRoot]);
 
     const toggleDartTheme = () => {
         setDarkThemeEnabled(prev => !prev);
@@ -29,9 +30,12 @@ export const DCHeader = () => {
                             <Link className='nav-link' to="/drone-game"><Text>Drone Game</Text></Link>
                         </Row>
                     </Col>
-                    <Col>
-                        <Toggle checked={darkThemeEnabled} onChange={toggleDartTheme}/>
-                    </Col>
+                    {
+                        isRoot ? null :
+                        <Col>
+                            <Toggle checked={darkThemeEnabled} onChange={toggleDartTheme}/>
+                        </Col>
+                    }
                 </Row>
             </Container>
         </TopBar>
