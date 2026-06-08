9547602e03683247657393fc380b09353a650d89
DigitalCrafting <k.kasprzyk2@gmail.com>
2026-06-08 11:00:48 +0200

    [digital-crafting] TextInputDocs

---
+++ apps/digital-crafting/src/pages/zoria-ui-demo/components/inputs/TextInputDocs.tsx
@@ -0,0 +1,28 @@
+import {CodeBlock, Col, Row, TextInput} from "@zoria-ui/react";
+import {ZoriaDocsPanel} from "../../zoria-docs-panel/ZoriaDocsPanel.tsx";
+
+export const TextInputDocs = () => {
+    return <ZoriaDocsPanel>
+        <ZoriaDocsPanel.Title>Text Input</ZoriaDocsPanel.Title>
+        <ZoriaDocsPanel.Body>
+            <ZoriaDocsPanel.Demo>
+                <Row>
+                    <Col span={5}>
+                        <TextInput placeholder='Funny placeholder' onChange={(value) => console.log(value)} label='Text input'/>
+                    </Col>
+                </Row>
+            </ZoriaDocsPanel.Demo>
+            <ZoriaDocsPanel.Code>
+                <CodeBlock>
+                    {/* language=text */}
+                    {`<TextInput placeholder='Funny placeholder' onChange={(value) => console.log(value)} label='Text input'/>`}
+                </CodeBlock>
+            </ZoriaDocsPanel.Code>
+            <ZoriaDocsPanel.Docs>
+                <Row className='text-center'>
+                    WIP
+                </Row>
+            </ZoriaDocsPanel.Docs>
+        </ZoriaDocsPanel.Body>
+    </ZoriaDocsPanel>
+}
