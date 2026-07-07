df7886f594bcba7889692185aa372f0cb1f72b87
DigitalCrafting <k.kasprzyk2@gmail.com>
2026-07-07 15:20:33 +0200

    [@zoria-ui] Fixed docs navigation highlight

--- apps/digital-crafting/src/pages/zoria-ui-demo/ZoriaUIDemoPage.tsx
+++ apps/digital-crafting/src/pages/zoria-ui-demo/ZoriaUIDemoPage.tsx
@@ -13,33 +13,36 @@ export const ZoriaUIDemoPage = () => {
             <Sidebar.Header>Zoria UI</Sidebar.Header>
             <Sidebar.Body>
                 <Navigation>
-                    <Navigation.Section defaultOpen title={'@zoria-ui/react'}>
-                        <Navigation.Item key={'about'} as={Link} active={pathname.endsWith('about')} to={`/zoria/about`}>About</Navigation.Item>
+                    <Navigation.Section defaultOpen={pathname.includes('/react')} title={'@zoria-ui/react'}>
+                        <Navigation.Item key={'about'} as={Link} active={pathname.endsWith('react/about')}
+                                         to={`/zoria/react/about`}>About</Navigation.Item>
                         {
                             ZoriaUIDocsRoutesList.map(route => {
-                                return <Navigation.Subsection key={route} title={StringUtils.capitalizeWord(route)}>
+                                return <Navigation.Subsection defaultOpen={pathname.includes(`/react/${route}`)}
+                                                              key={route} title={StringUtils.capitalizeWord(route)}>
                                     {
                                         ZoriaUIDocsRoutes[route]!.map((subroute) => {
-                                            return <Navigation.Item key={subroute} as={Link} active={pathname.endsWith(subroute)} to={`/zoria/${route}/${subroute}`}>{StringUtils.toPascalCase(subroute)}</Navigation.Item>
+                                            return <Navigation.Item key={subroute} as={Link}
+                                                                    active={pathname.endsWith(`react/${route}/${subroute}`)}
+                                                                    to={`/zoria/react/${route}/${subroute}`}>{StringUtils.toPascalCase(subroute)}</Navigation.Item>
                                         })
                                     }
                                 </Navigation.Subsection>
                             })
                         }
                     </Navigation.Section>
-                    <Navigation.Section defaultOpen={pathname.endsWith('/forms')} title={'@zoria-ui/forms'}>
-                        <Navigation.Item as={Link} active={pathname.endsWith('/about')} to='/zoria/forms/about'>About</Navigation.Item>
-                        <Navigation.Item as={Link} active={pathname.endsWith('/demo')} to='/zoria/forms/demo'>Demo</Navigation.Item>
-                        {/*<Navigation.Item as={Link} to='/zoria/forms/form-group'>Form Group</Navigation.Item>*/}
-                        {/*<Navigation.Item as={Link} to='/zoria/forms/validation'>Validation</Navigation.Item>*/}
-                        {/*<Navigation.Item as={Link} to='/zoria/forms/exmaples'>Examples</Navigation.Item>*/}
+                    <Navigation.Section defaultOpen={pathname.includes('/forms')} title={'@zoria-ui/forms'}>
+                        <Navigation.Item as={Link} active={pathname.endsWith('forms/about')}
+                                         to='/zoria/forms/about'>About</Navigation.Item>
+                        <Navigation.Item as={Link} active={pathname.endsWith('forms/demo')}
+                                         to='/zoria/forms/demo'>Demo</Navigation.Item>
                     </Navigation.Section>
                 </Navigation>
             </Sidebar.Body>
         </Sidebar>
         <Main>
             <Container>
-                <Row className={`justify-center`} >
+                <Row className={`justify-center`}>
                     <Col
                         className={styles.ZoriaUIDemoPage}
                     >
--- apps/digital-crafting/src/pages/zoria-ui-demo/config/ZoriaUIRoutesConfig.tsx
+++ apps/digital-crafting/src/pages/zoria-ui-demo/config/ZoriaUIRoutesConfig.tsx
@@ -33,134 +33,139 @@ import {ZoriaUiFormsAbout} from "../forms/ZoriaUiFormsAbout.tsx";
 
 export const ZoriaUIRoutesConfig: RouteObject[] = [
     {
-        path: `about`,
-        index: true,
-        Component: ZoriaUiReactAbout
-    },
-    {
-        path: `${ZoriaUIRoutePathsEnum.TYPOGRAPHY}`,
+        path: 'react',
         children: [
             {
-                path: `${ZoriaUISubroutesPathsEnum.TEXT}`,
-                Component: TextDocs
+                path: `about`,
+                index: true,
+                Component: ZoriaUiReactAbout
             },
             {
-                path: `${ZoriaUISubroutesPathsEnum.HEADERS}`,
-                Component: HeadersDocs
+                path: `${ZoriaUIRoutePathsEnum.TYPOGRAPHY}`,
+                children: [
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.TEXT}`,
+                        Component: TextDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.HEADERS}`,
+                        Component: HeadersDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.ICONS}`,
+                        Component: IconsDocs
+                    }
+                ]
             },
             {
-                path: `${ZoriaUISubroutesPathsEnum.ICONS}`,
-                Component: IconsDocs
+                path: `${ZoriaUIRoutePathsEnum.BUTTONS}`,
+                children: [
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.BUTTON}`,
+                        Component: ButtonDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.ICON_BUTTON}`,
+                        Component: IconButtonDocs
+                    }
+                ]
+            }, {
+                path: `${ZoriaUIRoutePathsEnum.INPUTS}`,
+                children: [
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.INPUT}`,
+                        Component: InputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.TEXT_INPUT}`,
+                        Component: TextInputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.TEXTAREA_INPUT}`,
+                        Component: TextareaInputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.NUMBER_INPUT}`,
+                        Component: NumberInputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.SELECT_INPUT}`,
+                        Component: SelectInputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.AUTOCOMPLETE_INPUT}`,
+                        Component: AutocompleteInputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.EMAIL_INPUT}`,
+                        Component: EmailInputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.PASSWORD_INPUT}`,
+                        Component: PasswordInputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.DATE_PICKER_INPUT}`,
+                        Component: DatePickerInputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.TIME_PICKER_INPUT}`,
+                        Component: TimePickerInputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.DATE_TIME_PICKER_INPUT}`,
+                        Component: DateTimePickerInputDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.CALENDAR}`,
+                        Component: CalendarDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.CHECKBOX}`,
+                        Component: CheckboxDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.TOGGLE}`,
+                        Component: ToggleDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.RADIO_GROUP}`,
+                        Component: RadioGroupDocs
+                    },
+                ]
+            }, {
+                path: `${ZoriaUIRoutePathsEnum.DISPLAY}`,
+                children: [
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.CHIP}`,
+                        Component: ChipDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.SPINNER}`,
+                        Component: SpinnerDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.TABLE}`,
+                        Component: TableDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.TABS}`,
+                        Component: TabsDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.TOOLTIP}`,
+                        Component: TooltipDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.POPOVER}`,
+                        Component: PopoverDocs
+                    },
+                    {
+                        path: `${ZoriaUISubroutesPathsEnum.MODAL}`,
+                        Component: ModalDocs
+                    },
+                ]
             }
-        ]
-    },
-    {
-        path: `${ZoriaUIRoutePathsEnum.BUTTONS}`,
-        children: [
-            {
-                path: `${ZoriaUISubroutesPathsEnum.BUTTON}`,
-                Component: ButtonDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.ICON_BUTTON}`,
-                Component: IconButtonDocs
-            }
-        ]
-    }, {
-        path: `${ZoriaUIRoutePathsEnum.INPUTS}`,
-        children: [
-            {
-                path: `${ZoriaUISubroutesPathsEnum.INPUT}`,
-                Component: InputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.TEXT_INPUT}`,
-                Component: TextInputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.TEXTAREA_INPUT}`,
-                Component: TextareaInputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.NUMBER_INPUT}`,
-                Component: NumberInputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.SELECT_INPUT}`,
-                Component: SelectInputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.AUTOCOMPLETE_INPUT}`,
-                Component: AutocompleteInputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.EMAIL_INPUT}`,
-                Component: EmailInputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.PASSWORD_INPUT}`,
-                Component: PasswordInputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.DATE_PICKER_INPUT}`,
-                Component: DatePickerInputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.TIME_PICKER_INPUT}`,
-                Component: TimePickerInputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.DATE_TIME_PICKER_INPUT}`,
-                Component: DateTimePickerInputDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.CALENDAR}`,
-                Component: CalendarDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.CHECKBOX}`,
-                Component: CheckboxDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.TOGGLE}`,
-                Component: ToggleDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.RADIO_GROUP}`,
-                Component: RadioGroupDocs
-            },
-        ]
-    }, {
-        path: `${ZoriaUIRoutePathsEnum.DISPLAY}`,
-        children: [
-            {
-                path: `${ZoriaUISubroutesPathsEnum.CHIP}`,
-                Component: ChipDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.SPINNER}`,
-                Component: SpinnerDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.TABLE}`,
-                Component: TableDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.TABS}`,
-                Component: TabsDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.TOOLTIP}`,
-                Component: TooltipDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.POPOVER}`,
-                Component: PopoverDocs
-            },
-            {
-                path: `${ZoriaUISubroutesPathsEnum.MODAL}`,
-                Component: ModalDocs
-            },
         ]
     },
     {
